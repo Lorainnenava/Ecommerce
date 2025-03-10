8fbddd14181e623c33d1b7d8d4771d7cfff57af3
Lorainnenava <lorainnenavarro0723@gmail.com>
2025-03-10 17:08:36 -0500

    Add data context and migration

---
+++ Ecommerce.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Shared.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int CountryId { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage ="El campo {0} no puuede tener más de {1} caractéres")]
+        public string Name { get; set; } = null!;
+    }
+}
