7b296f4783da07af4f94e4c34e3a47e1a28a4e20
Lorainnenava <lorainnenavarro0723@gmail.com>
2025-03-11 08:25:43 -0500

    Begin countries API controller

---
+++ Ecommerce.API/Controllers/CountriesController.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.API.Data;
+using Ecommerce.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
+
